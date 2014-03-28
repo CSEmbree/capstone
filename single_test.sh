8c6bbf272263018ab8fef17d3bea1a293813e16b
csembree <csembree@gmail.com>
2014-03-28 22:47:04 +0000

    examining feature vector options

--- single_test.sh
+++ single_test.sh
@@ -1,14 +1,8 @@
-B1;2c#!/bin/bash
+#!/bin/bash
 
 fpath=$(pwd)"/rec/"
 fname="rec_single_TEST."
 fext=".wav"
 
-arecord -D plughw:1 --duration=10 -f cd -vv $fpath$fname$(date +"%Y:%m:%d:%S:%N")$fext
-
-
-
-
-
-
+arecord -D plughw:1 --duration=2 -f cd -vv $fpath$fname$(date +"%Y:%m:%d:%S:%N")$fext
 
