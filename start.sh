00ac77f92471ccc0836a12cce40a89d2f0aa2d5c
cameron embree <csembree@gmail.com>
2014-09-26 00:49:18 +0000

    new start script type including some yaafe env vars

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 export SOUND_BASE_DIR=/home/pi/sounds
+export YAAFE=$SOUND_BASE_DIR/yaafe-v0.64
+export YAAFE_PATH=$YAAFE/yaafe_extensions
+export PATH=$PATH:$YAAFE/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$YAAFE/lib
+export PYTHONPATH=$PYTHONPATH:$YAAFE/python_packages
 ./raraa
