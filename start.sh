057010ed0fc2a64e6ea77a791c2042543c6b7fc9
cameron embree <csembree@gmail.com>
2014-10-11 04:25:32 +0000

    removed old start and stops. These are genreated by installer.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export SOUND_BASE_DIR=/home/pi/sounds
-export YAAFE=$SOUND_BASE_DIR/yaafe-v0.64
-export YAAFE_PATH=$YAAFE/yaafe_extensions
-export PATH=$PATH:$YAAFE/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$YAAFE/lib
-export PYTHONPATH=$PYTHONPATH:$YAAFE/python_packages
-(cd $SOUND_BASE_DIR && ./raraa)
