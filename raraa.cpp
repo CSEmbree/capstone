b49c42bf47a072c77bfacddf1373ea6933fa7ac9
cameron embree <csembree@gmail.com>
2014-11-24 00:55:26 +0000

    Now supports overloading of config file path, num runs, and duration in any order. Also supports change in output format from FV,YAAFE to WRAPPED,FILES.

--- raraa.cpp
+++ raraa.cpp
@@ -36,7 +36,7 @@ bool do_filter_feature_extraction( config_handler *ch, audio_recorder *ar ) {
 	    << " "    << ar->get_rec_file_name();
   //        << " -b " << ch.get_analysis_location(); // TODO - change output location ?
   
-  //perform FV element extraction
+  //perform feature extraction
   system(filterCmd.str().c_str());
   
   //alert user to cration of feature vector elements
@@ -170,8 +170,8 @@ void simulate_run( config_handler *ch ) {
 
 
   string extentions = "";
-  if ( ch->get_final_feature_format() == "FV" )         extentions = "{*.wav,*.dat}";
-  else if ( ch->get_final_feature_format() == "YAAFE" ) extentions = "{*.wav,*.dat,*.csv}";
+  if ( ch->get_final_feature_format() == "WRAPPED" )    extentions = "{*.wav,*.dat}";
+  else if ( ch->get_final_feature_format() == "FILES" ) extentions = "{*.wav,*.dat,*.csv}";
   else                                                  extentions = "*";
 
 
@@ -237,8 +237,8 @@ bool clean_analysis_workspace( string timeStamp, config_handler *ch, audio_recor
 int main(int argc, char **argv) {
 
   string mn = "main:";
-  int dur = 5;         //HARDCODED TEMP RECORD LENGTH IF NONE SELECTED BY USER
-  int numRuns = 1;     //HARDCODED TEMP NUMBER OF RUNS
+  int dur = -1;         //HARDCODED TEMP RECORD LENGTH IF NONE SELECTED BY USER
+  int numRuns = -1;     //HARDCODED TEMP NUMBER OF RUNS
 
 
   //******************CONFIG PHASE******************
@@ -247,36 +247,55 @@ int main(int argc, char **argv) {
   string configPath = utils::get_home_dir();//"/home/pi/sounds";
   string configFname = "cirainbow.conf";
 
+
+  // allow user to choose a specifc config file by argument
+  int numCheck = 0;
+  if(argc >= 1) {
+    
+    for(int i = 1; i < argc; i++ ) {
+      string curArg = argv[i];
+      
+      // last readable string is assumed to be a config file
+      if ( utils::is_file_readable( curArg ) == true ) {
+	configPath = utils::get_cwd() + utils::get_directory_path( curArg );
+	configFname = utils::get_base( curArg );
+
+      } else if ( utils::is_pos_int( curArg ) == true && numCheck == 0 ) {
+	// first pos int found is assumed to be duration
+	dur = utils::string_to_number<int>( curArg );
+	numCheck++;
+      
+      } else if ( utils::is_pos_int( curArg ) == true && numCheck == 1 ) {
+	// first pos int found is assumed to be number of recordings
+	numRuns = utils::string_to_number<int>( curArg );
+	numCheck++;
+      }
+    }
+  }
+  
+  
+
+  // We need a configuration file manager to ask questions about our recording state later
   cout<<n<<mn<<" Started reading config file ... "<<endl;
   config_handler ch( configPath, configFname );
   cout<<n<<mn<<" Finished reading config file."<<endl;
 
 
-  if(ch.get_simulate() == true) simulate_run( &ch ); 
-
+  // Running a simulation or in background handled first as they dictate later behavior
+  if( ch.get_simulate()   == true ) simulate_run( &ch ); 
   if( ch.get_background() == true ) utils::daemonize();
+  
+  // User requested number of recordings and duration take presidence over config file settings
+  if( dur     != -1 ) ch.set_rec_duration( dur );
+  if( numRuns != -1 ) ch.set_rec_number( numRuns );
 
 
+  // create an audio recorder with user's settings
   cout<<n<<mn<<" Creating audio recorder for '"<<ch.get_rec_number()
       <<"' runs, each '"<<ch.get_rec_duration()<<"' sec(s) ... "<<endl; 
-  
   audio_recorder ar( ch );
   cout<<n<<mn<<" Created audio recorder."<<endl;
 
-
-  
-  // allow user to overload number of runs and run duration
-  if(argc >= 3) {
-    
-    cout<<n<<mn<<" Overloading audio recording settings...";
-
-    numRuns = atoi(argv[1]); // arg1 - user num of runs ('0' is inf runs (arecord(1)).
-    dur     = atoi(argv[2]); // arg2 - user duration for recording 
-    ch.set_rec_number( numRuns );
-    ch.set_rec_duration( dur );
-  }
-
-  
   
   //Alert user to starting of recording with desired duration
   cout<<n<<mn<<" Making '"<<ch.get_rec_number()
@@ -294,12 +313,15 @@ int main(int argc, char **argv) {
   int recRunCount  = 0;  // run counter
   bool forever = false;
 
+  
+  // The number of recording can be finite or infinite (if zero)
   if( ch.get_rec_number() == 0 ) forever = true;
   if( ch.get_rec_number() < 0 ) { 
     cout<<n<<mn<<" WARNING: Invalid number of recordings requested. Check config file: \""
 	<<ch.get_config_file()<<"\"."<<endl;
     exit( 0 ); //invalid
   }
+
 
   //Make as many recordings as requested
   for( recRunCount=0; recRunCount < ch.get_rec_number() || forever ; recRunCount++ ) {
@@ -342,82 +364,76 @@ int main(int argc, char **argv) {
   
   // Child takes care of feature extraction
 
-  if( ch.get_analysis() == true ) {    
+  if( childpid == 0 ) {
     
-    if( childpid == 0 ) {
-      
-      bool interesting = true; // assume all audio is interesting unless we find otherwise
-      
-      
-      //---------FILTER FEATURE EXTRACTION----------
-      // Do minimal feature extractions for filtering requirments
-      if( ch.get_filter() == true ) {
-	cout<<n<<mn<<" Performing basic audio filtering ... "<<endl;
-	do_filter_feature_extraction( &ch, &ar );
-	cout<<n<<mn<<" Extracted filter features (child pid \""<<(long)getpid()<<"\")."<<endl;
-	
-        
-	//---------ANALYIZE FILTER FEATURES----------
-	// Analyize the features for a filter to look for interesting features
-	interesting = filters::perceptual_sharpness(ar.get_rec_file_name()+".ps.csv");    
-	cout<<n<<mn<<" Completed filter analysis (child pid \""<<(long)getpid()<<"\")."
-	    <<" Interesting: "<<(interesting? "YES!":"NO!")<<endl; 
-      } else {
-	cout<<n<<mn<<" No feature filtering is being performed. "<<endl;
-      }    
+    bool interesting = true; // assume all audio is interesting unless we find otherwise
+    
+    
+    //---------FILTER FEATURE EXTRACTION----------
+    // Do minimal feature extractions for filtering requirments
+    if( ch.get_filter() == true ) {
+      cout<<n<<mn<<" Performing basic audio filtering ... "<<endl;
+      do_filter_feature_extraction( &ch, &ar );
+      cout<<n<<mn<<" Extracted filter features (child pid \""<<(long)getpid()<<"\")."<<endl;
       
       
-      
-      //---------EXTRACT MORE FEATURES IF INTERSTING----------
-      // if a feature was interesting we need to extract more features and deploy them
-      if( interesting == true ) {
-	do_feature_extraction( &ch, &ar );    
-	cout<<n<<mn<<" Completed full feature extraction (child pid \""<<(long)getpid()<<"\")."<<endl;
-      }
-      
-      
-      
-      //---------MOVE EXTRACTED FEATURES TO DEPLOYMENT----------
-      // Move features extracted to deployment based on user's desired format
-      string dataFormat = ch.get_final_feature_format();
-      cout<<n<<mn<<" Desired format is: \""<<dataFormat<<"\""<<endl;
-      
-
-      // Keeping or disgarding audio is not a function of the final data format
-      if( ch.get_save_rec() == true ) {
-	move_audio( &ch, &ar ); 
-      }      
+      //---------ANALYIZE FILTER FEATURES----------
+      // Analyize the features for a filter to look for interesting features
+      interesting = filters::perceptual_sharpness(ar.get_rec_file_name()+".ps.csv");    
+      cout<<n<<mn<<" Completed filter analysis (child pid \""<<(long)getpid()<<"\")."
+	  <<" Interesting: "<<(interesting? "YES!":"NO!")<<endl; 
+    } else {
+      cout<<n<<mn<<" No feature filtering is being performed. "<<endl;
+    }    
+    
+    
+    
+    //---------EXTRACT MORE FEATURES IF INTERSTING----------
+    // if a feature was interesting we need to extract more features and deploy them
+    if( interesting == true ) {
+      do_feature_extraction( &ch, &ar );    
+      cout<<n<<mn<<" Completed full feature extraction (child pid \""<<(long)getpid()<<"\")."<<endl;
+    }
+    
+    
+    
+    //---------MOVE EXTRACTED FEATURES TO DEPLOYMENT----------
+    // Move audio and features extracted to data deployment based on user's desired formats
 
 
+    // Keeping or disgarding audio is not a function of the final data format
+    if( ch.get_save_rec() == true ) {
+      move_audio( &ch, &ar ); 
+    }      
+    
+    
+    
+    //---------CREATE A Feature Vector & metadata from extracted features----------
+    // create a feature vector as json foramtted file
+    cout<<n<<mn<<" Creating a feature vector ... "<<endl;      
+    feature_vector fv( timeStamp, &ch, &ar ); //TEST
+    fv.write( &ch, &ar );
+    
+    
+    
+    if( ch.get_final_feature_format() == "FILES" ) {
+      // Move all features with a helpful meta data file if not creating a feature vector
+      cout<<n<<mn<<" Moving features extracted to deployment ... "<<endl;
+      
+      move_features( &ch, &ar ); //blindly move features to data	
+    }    
+    
+    
+    cout<<" Finished moving features and meta data to deployment. "<<"(child pid \""<<(long)getpid()<<"\")."<<endl;
+    
 
-      //---------CREATE A Feature Vector & metadata from extracted features----------
-      // create a feature vector as json foramtted file
-      cout<<n<<mn<<" Creating a feature vector ... "<<endl;      
-      feature_vector fv( timeStamp, &ch, &ar ); //TEST
-      fv.write( &ch, &ar );
-      
-      
-      
-      if( dataFormat == "YAAFE" ) {
-	// Move all features with a helpful meta data file if not creating a feature vector
-	cout<<n<<mn<<" Moving features extracted to deployment ... "<<endl;
-	
-	move_features( &ch, &ar ); //blindly move features to data	
-      }    
-      
-
-      cout<<" Finished moving features and meta data to deployment. "
-	  <<"(child pid \""<<(long)getpid()<<"\")."<<endl;
-      
-
-      
-      clean_analysis_workspace( timeStamp, &ch, &ar );
-      
-      
-      cout<<n<<mn<<" Audio Analysis complete (child pid \""<<(long)getpid()<<"\")."<<endl;
-    } //end child analysis process check
-  } //end config analysis check
-
+    // delete all local files after user requested ones are moved to data deployment directory
+    clean_analysis_workspace( timeStamp, &ch, &ar );
+    
+    
+    cout<<n<<mn<<" Audio Analysis complete (child pid \""<<(long)getpid()<<"\")."<<endl;
+  } //end child analysis process check
+  
   
   //Handle child processes based on their job
   if(childpid == 0) {
@@ -431,7 +447,7 @@ int main(int argc, char **argv) {
   }
   
 
-  cout<<n<<mn<<" ERROR: Raraa core finished: This should never be seen! "<<endl;
+  cerr<<n<<mn<<" ERROR: Raraa core finished: This should never be seen! "<<endl;
 
 
   return 0;
